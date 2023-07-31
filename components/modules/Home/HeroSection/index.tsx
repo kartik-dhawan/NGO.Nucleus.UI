81ad1aa6a7b8cd97b714eeefde3d58f6546ef626
kartik-dhawan <kartikkdhawan@gmail.com>
2023-07-31 14:21:20 +0530

    N-06: Added responsive styles for hero section

--- components/modules/Home/HeroSection/index.tsx
+++ components/modules/Home/HeroSection/index.tsx
@@ -3,48 +3,36 @@ import { RootType } from "../../../../redux/store"
 import { Box, Typography } from "@mui/material"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Image from "next/image"
+import { styles } from "./styles"
 
 const HeroSection = () => {
   const { content } = useSelector((state: RootType) => state.contentSlice)
 
   return (
-    <Box
-      component="section"
-      sx={{ display: "flex", flexDirection: "column", gap: "10rem" }}
-    >
+    <Box component="section" sx={styles.heroSectionWrapper}>
       {content.heroSectionImages &&
         content.heroSectionImages.map((item: any, index: number) => {
           return (
             <Box
               key={item.sys.id}
               sx={{
-                display: "flex",
-                gap: "5rem",
-                flexDirection: index === 0 ? "row" : "row-reverse",
-                position: "relative",
+                ...styles.heroSectionItemWrapper,
+                flexDirection:
+                  index === 0
+                    ? {
+                        xs: "column-reverse",
+                        md: "row",
+                      }
+                    : {
+                        xs: "column-reverse",
+                        md: "row-reverse",
+                      },
               }}
             >
-              <Box
-                sx={{
-                  flex: 4,
-                  padding: "24px 32px",
-                  boxSizing: "border-box",
-                }}
-              >
+              <Box sx={styles.heroSectionTypographyWrapper}>
                 <Typography
                   className="barlowCondensed"
-                  sx={{
-                    position: "sticky",
-                    top: "78px",
-                    // left: 0,
-                    "& > h1": {
-                      margin: "4px 0px",
-                      fontSize: "32px",
-                    },
-                    "& p": {
-                      fontSize: "18px",
-                    },
-                  }}
+                  sx={styles.heroSectionTypography}
                 >
                   {index === 0 &&
                     documentToReactComponents(content?.heroLandingSectionBody)}
@@ -53,15 +41,7 @@ const HeroSection = () => {
                 </Typography>
                 <Box></Box>
               </Box>
-              <Box
-                sx={{
-                  flex: 5,
-                  "& > img": {
-                    width: "100%",
-                    height: "100%",
-                  },
-                }}
-              >
+              <Box sx={styles.heroSectionImages}>
                 {content?.heroSectionImages && (
                   <Image
                     src={
@@ -71,7 +51,7 @@ const HeroSection = () => {
                     alt={content?.heroSectionImages[index].fields.description}
                     width={2787}
                     height={4180}
-                  ></Image>
+                  />
                 )}
               </Box>
             </Box>
--- components/modules/Home/HeroSection/styles/index.ts
+++ components/modules/Home/HeroSection/styles/index.ts
@@ -1,1 +1,62 @@
-export const styles = {}
+export const styles = {
+  heroSectionWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    gap: {
+      xs: "2rem",
+      md: "12rem",
+    },
+  },
+  heroSectionItemWrapper: {
+    display: "flex",
+    gap: {
+      sm: "2rem",
+      lg: "10rem",
+    },
+    position: "relative",
+  },
+  heroSectionTypographyWrapper: {
+    flex: 4,
+    padding: "24px 32px",
+    boxSizing: "border-box",
+  },
+  heroSectionTypography: {
+    position: {
+      md: "sticky",
+    },
+    top: "78px",
+    "& > h1": {
+      margin: "4px 0px",
+      fontSize: {
+        xs: "32px",
+        xl: "38px",
+      },
+    },
+    "& p": {
+      fontSize: {
+        xs: "18px",
+        xl: "21px",
+      },
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      display: "-webkit-box",
+      WebkitLineClamp: {
+        md: 5,
+        lg: 7,
+      },
+      WebkitBoxOrient: "vertical",
+    },
+  },
+  heroSectionImages: {
+    flex: 6,
+    "& > img": {
+      width: "100%",
+      height: {
+        xs: "100vh",
+        md: "100%",
+      },
+      objectFit: "cover",
+      objectPosition: "50% 30%",
+    },
+  },
+}
