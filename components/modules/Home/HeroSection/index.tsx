38efaaad697220c359a22860e29c8654bdc6326d
kartik-dhawan <kartikkdhawan@gmail.com>
2023-07-31 14:57:20 +0530

    N-06: Wrote UTs

--- components/modules/Home/HeroSection/index.tsx
+++ components/modules/Home/HeroSection/index.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 import { RootType } from "../../../../redux/store"
-import { Box, IconButton, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Image from "next/image"
 import { styles } from "./styles"
@@ -11,60 +11,56 @@ const HeroSection = () => {
 
   return (
     <Box component="section" sx={styles.heroSectionWrapper}>
-      {content.heroSectionImages &&
-        content.heroSectionImages.map((item: any, index: number) => {
-          return (
-            <Box
-              key={item.sys.id}
-              sx={{
-                ...styles.heroSectionItemWrapper,
-                flexDirection:
-                  index === 0
-                    ? {
-                        xs: "column-reverse",
-                        md: "row",
-                      }
-                    : {
-                        xs: "column-reverse",
-                        md: "row-reverse",
-                      },
-              }}
-            >
-              <Box sx={styles.heroSectionTypographyWrapper}>
-                <Typography
-                  className="barlowCondensed"
-                  sx={styles.heroSectionTypography}
-                >
-                  {index === 0 &&
-                    documentToReactComponents(content?.heroLandingSectionBody)}
-                  {index === 1 &&
-                    documentToReactComponents(content?.heroQuoteSectionBody)}
-                  <IconButton
-                    disableRipple
-                    sx={styles.heroLandingSectionButton}
-                  >
-                    {content.heroLandingSectionButtonText[index]}{" "}
-                    <ArrowRightAltIcon />
-                  </IconButton>
-                </Typography>
-                <Box></Box>
-              </Box>
-              <Box sx={styles.heroSectionImages}>
-                {content?.heroSectionImages && (
-                  <Image
-                    src={
-                      "https:" +
-                      content?.heroSectionImages[index].fields?.file.url
+      {content.heroSectionImages?.map((item: any, index: number) => {
+        return (
+          <Box
+            key={item.sys.id}
+            sx={{
+              ...styles.heroSectionItemWrapper,
+              flexDirection:
+                index === 0
+                  ? {
+                      xs: "column-reverse",
+                      md: "row",
                     }
-                    alt={content?.heroSectionImages[index].fields.description}
-                    width={2787}
-                    height={4180}
-                  />
-                )}
-              </Box>
+                  : {
+                      xs: "column-reverse",
+                      md: "row-reverse",
+                    },
+            }}
+          >
+            <Box sx={styles.heroSectionTypographyWrapper}>
+              <Typography
+                className="barlowCondensed"
+                sx={styles.heroSectionTypography}
+              >
+                {index === 0 &&
+                  documentToReactComponents(content?.heroLandingSectionBody)}
+                {index === 1 &&
+                  documentToReactComponents(content?.heroQuoteSectionBody)}
+                <Button disableRipple sx={styles.heroLandingSectionButton}>
+                  {content.heroLandingSectionButtonText[index]}{" "}
+                  <ArrowRightAltIcon />
+                </Button>
+              </Typography>
+              <Box></Box>
             </Box>
-          )
-        })}
+            <Box sx={styles.heroSectionImages}>
+              {content?.heroSectionImages && (
+                <Image
+                  src={
+                    "https:" +
+                    content?.heroSectionImages[index].fields?.file.url
+                  }
+                  alt={content?.heroSectionImages[index].fields.description}
+                  width={2787}
+                  height={4180}
+                />
+              )}
+            </Box>
+          </Box>
+        )
+      })}
     </Box>
   )
 }
